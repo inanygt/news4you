8b381e0178c1c968004913b736acc45b264aa78e
JORIAN KASPRZAK <joriankasprzak@hotmail.com>
2023-01-23 15:06:55 +0100

    search api feed commit

--- src/app/newsfeedtopic/newsfeedtopic.component.ts
+++ src/app/newsfeedtopic/newsfeedtopic.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NewsApiService } from '../services/news-api-service';
 
 @Component({
   selector: 'app-newsfeedtopic',
   templateUrl: './newsfeedtopic.component.html',
   styleUrls: ['./newsfeedtopic.component.css']
 })
-export class NewsfeedtopicComponent {
+
+
+export class NewsfeedtopicComponent implements OnInit {
+
+
+
+  // urlLink: string = 'https://newsapi.org/v2/everything?q='
+  // urlKey: string = '&apiKey=28708e155adf4043b69f678b8f7c75d9'
+  searchWord: string = ''
+  // searchLink = this.urlLink + this.keyWord + this.urlKey;
+
+  searchData: any [] = []
+  searchTerm: string = '';
+  searchDataLink: string ='';
+
+  search() {
+    this.searchWord = this.searchTerm;
+    this.NewsApiService.searchLink = this.NewsApiService.urlLink + this.searchTerm + this.NewsApiService.urlKey;
+    this.searchDataLink = this.NewsApiService.searchLink
+    console.log(this.searchTerm)
+    console.log(this.NewsApiService.searchLink)
+    fetch(this.NewsApiService.searchLink).then(res => res.json())
+
+
+  }
+
+  constructor(private NewsApiService: NewsApiService) {
+
+  }
+
+  ngOnInit(): void {
+    this.NewsApiService.getSearch().then((data: any) => {
+      // console.log(data.articles)
+      this.searchData = data.articles
+        // console.log(this.searchData)
+        // console.log(this.searchLink)
+
+    })
+
+  }
 
 }
+
+
+
+
+
+
+
--- src/app/services/news-api-service.ts
+++ src/app/services/news-api-service.ts
@@ -1,11 +1,15 @@
 import { Injectable, NgModule } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { getLocaleDateFormat } from '@angular/common';
+import { NewsfeedtopicComponent } from '../newsfeedtopic/newsfeedtopic.component';
+
 
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class NewsApiService {
  url: string = 'https://newsapi.org/v2/everything?q=keyword&apiKey=28708e155adf4043b69f678b8f7c75d9'
 
@@ -13,18 +17,40 @@ export class NewsApiService {
  constructor(){}
 
  getData(){
-
   return fetch(this.url).then(res => res.json())
+ }
 
+ urlLink: string = 'https://newsapi.org/v2/everything?q='
+ urlKey: string = '&apiKey=28708e155adf4043b69f678b8f7c75d9'
+ keyWord: string = ""
+ searchLink: string = this.urlLink + this.keyWord + this.urlKey;
+
+ searchData: any [] = []
+
+search(){
+
+}
+
+ getSearch(){
+   return fetch(this.searchLink).then(res => res.json())
 
  }
 
-
-
-
-  // .then(data => console.log(data))
-
-
 }
+
+// export class newsfeedtopics {
+//   urlLink: string = 'https://newsapi.org/v2/everything?q='
+//   urlKey: string = '&apiKey=28708e155adf4043b69f678b8f7c75d9'
+//   keyWord: string = 'bitcoin';
+//   searchLink = this.urlLink + this.keyWord + this.urlKey;
+
+//   searchData: any [] = []
+//   constructor(){}
+
+//   getSearch(){
+//     return fetch(this.searchLink).then(res => res.json)
+//   }
+
+
 
 
