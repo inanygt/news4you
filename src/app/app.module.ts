4f8850d02faa4a32728637bc9c447d03ded00126
Inan Yigit <inanygt@hotmail.com>
2023-01-24 13:57:38 +0100

    succcesfull merge verificationlogin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
+// DEPENDENCIES
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
+
+// COMPONENTS
 import { AppComponent } from './app.component';
 import { WelcomePageComponent } from '././components/welcome-page/welcome-page.component';
-
 import { LoginComponent } from '././components/login/login.component';
-
 import { NewsfeedComponent } from '././components/newsfeed/newsfeed.component';
 import { NewsfeedGlobalComponent } from '././components/newsfeed-global/newsfeed-global.component';
 import { NewsfeedpersComponent } from '././components/newsfeedpers/newsfeedpers.component';
 import { NewsfeedtopicComponent } from '././components/newsfeedtopic/newsfeedtopic.component';
-import { FormsModule } from '@angular/forms';
 import { TopicsComponent } from '././components/topics/topics.component';
 import { HomeComponent } from '././components/home/home.component';
 import { SignupComponent } from '././components/signup/signup.component';
