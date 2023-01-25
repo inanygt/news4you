da00fc208cfed1b5193c0dc16a7601045645c062
Inan Yigit <inanygt@hotmail.com>
2023-01-25 20:03:29 +0100

    added toggle background

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
-
+  toggleDarkMode() {
+    var element = document.body;
+    element.classList.toggle('dark-mode');
+  }
 }
