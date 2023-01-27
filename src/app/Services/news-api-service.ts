3cc282f72cb74c601863efa54f3a5d95922d7c59
Inan Yigit <inanygt@hotmail.com>
2023-01-27 15:26:36 +0100

    merged 2 apis

--- src/app/Services/news-api-service.ts
+++ src/app/Services/news-api-service.ts
@@ -9,7 +9,10 @@ import { NewsfeedtopicComponent } from '../components/newsfeedtopic/newsfeedtopi
 })
 export class NewsApiService {
   url: string =
-    'https://newsapi.org/v2/everything?q=keyword&apiKey=28708e155adf4043b69f678b8f7c75d9';
+    'https://newsapi.org/v2/everything?q=keyword&apiKey=712dd0a6c8324f769ff3b97bd5590776';
+
+  // 712dd0a6c8324f769ff3b97bd5590776
+  // 28708e155adf4043b69f678b8f7c75d9
 
   newsdataApi =
     'https://newsdata.io/api/1/news?apikey=pub_1615436c42d2454a9e02ae00e9d51b6cb33e3&q=pegasus&language=en';
@@ -18,18 +21,21 @@ export class NewsApiService {
   constructor() {}
 
   getData() {
-    return fetch(this.newsdataApi).then((res) => res.json());
-    return fetch(this.url).then((res) => res.json());
+    // Merge 2 apis test
+
+    return fetch(this.url).then((response) => response.json());
+
+    // return fetch(this.newsdataApi).then((res) => res.json());
+    // return fetch(this.url).then((res) => res.json());
   }
 
   urlLink: string = 'https://newsapi.org/v2/everything?q=';
   urlKey: string = '&apiKey=28708e155adf4043b69f678b8f7c75d9';
   keyWord: string = '';
   searchLink: string = this.urlLink + this.keyWord + this.urlKey;
+  mergedData!: any;
 
   searchData: any[] = [];
-
-  // search() {}
 
   getSearch() {
     return fetch(this.searchLink).then((res) => res.json());
--- src/app/components/newsfeed/newsfeed.component.ts
+++ src/app/components/newsfeed/newsfeed.component.ts
@@ -8,6 +8,10 @@ import { NewsApiService } from 'src/app/Services/news-api-service';
 })
 export class NewsfeedComponent implements OnInit {
   newdata: any = [];
+  mergedApi: any = [];
+
+  newsdataApi =
+    'https://newsdata.io/api/1/news?apikey=pub_1615436c42d2454a9e02ae00e9d51b6cb33e3&q=pegasus&language=en';
 
   constructor(private NewsApiService: NewsApiService) {}
 
@@ -16,8 +20,16 @@ export class NewsfeedComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.NewsApiService.getData().then((data: any) => {
-      console.log(data);
+    this.NewsApiService.getData().then((data1) => {
+      // Read the contents of the second JSON file
+      fetch(this.newsdataApi)
+        .then((response) => response.json())
+        .then((data2) => {
+          // Merge the contents of the two JSON files
+          const mergedData = Object.assign(data1.articles, data2.results);
+          this.mergedApi = mergedData;
+          console.log(this.mergedApi);
+        });
     });
   }
 }
