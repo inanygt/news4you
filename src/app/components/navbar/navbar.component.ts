ea6091137e65227a608d9fde9187239e17a7747a
Inan Yigit <inanygt@hotmail.com>
2023-01-31 20:26:49 +0100

    top search working

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NewsApiService } from 'src/app/Services/news-api-service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  searchWord: string = '';
+  searchData: any[] = [];
+  searchTerm: string = '';
+  searchDataLink: string = '';
+
   toggleDarkMode() {
     var element = document.body;
     element.classList.toggle('dark-mode');
@@ -14,4 +21,45 @@ export class NavbarComponent {
   logout() {
     localStorage.removeItem('userId');
   }
+
+  search() {
+    this.searchWord = this.searchTerm;
+    this.NewsApiService.searchLink =
+      this.NewsApiService.urlLink +
+      this.searchTerm +
+      this.NewsApiService.urlKey;
+    this.searchDataLink = this.NewsApiService.searchLink;
+    this.NewsApiService.getSearch().then((data: any) => {
+      this.searchData = data.articles;
+    });
+  }
+
+  url = 'http://localhost:8000/api/';
+
+  bookmarkMe(title: any, url: any) {
+    // Get user id from local storage
+    window.localStorage.getItem('userName');
+    // Bookmark object
+    let bookmarked = {
+      title: title,
+      url: url,
+      user_id: window.localStorage.getItem('userId'),
+    };
+    console.log(bookmarked);
+    // Toast pop up
+    this.toastr.success('Woohoo!', 'Bookmarked!');
+    // Add bookmark to database
+    fetch(this.url + 'bookmarks', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(bookmarked),
+    });
+  }
+
+  constructor(
+    private NewsApiService: NewsApiService,
+    private toastr: ToastrService
+  ) {}
 }
