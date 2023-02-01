c549757941e0997cc0999f11cebdef930e03af31
Inan Yigit <inanygt@hotmail.com>
2023-02-01 11:23:17 +0100

    fixed errors

--- src/app/components/topics/topics.component.ts
+++ src/app/components/topics/topics.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FormBuilder, FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormArray,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-topics',
   templateUrl: './topics.component.html',
-  styleUrls: ['./topics.component.css']
+  styleUrls: ['./topics.component.css'],
 })
-
-
-export class TopicsComponent{
+export class TopicsComponent {
   form = new FormGroup({
     Sports: new FormControl(false),
     Politics: new FormControl(false),
@@ -22,15 +26,10 @@ export class TopicsComponent{
     // other formControls here
   });
 
-
   submitForm() {
     let selectedValues = Object.entries(this.form.value)
       .filter(([key, value]) => value)
       .map(([key, value]) => key);
     console.log(selectedValues);
   }
-
-
-
 }
-
