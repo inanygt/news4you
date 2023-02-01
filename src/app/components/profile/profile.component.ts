4a34424d35c27913c5020dcb983429960f93cd14
Inan Yigit <inanygt@hotmail.com>
2023-02-01 14:50:07 +0100

    logout btn working

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,36 +1,5 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-profile',
-//   templateUrl: './profile.component.html',
-//   styleUrls: ['./profile.component.css']
-// })
-// export class ProfileComponent {
-
-//   users: any;
-
-//   constructor() { }
-
-//   ngOnInit() {
-//     fetch('http://localhost:8000/api/users')
-//       .then(response => response.json())
-//       .then(data => {
-//         this.users = data;
-//         console.log(this.users)
-
-//         let user = JSON.parse(localStorage.getItem('user')!);
-//         console.log(user)
-//         console.log(user[0].userName);
-//         let username = user[0].userName
-//         console.log(username)
-
-//       });
-
-//   }
-
-// }
-
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -42,7 +11,7 @@ export class ProfileComponent implements OnInit {
 
   users: any;
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {
     fetch('http://localhost:8000/api/users')
@@ -69,5 +38,6 @@ export class ProfileComponent implements OnInit {
 
   logout() {
     localStorage.clear();
+    this.router.navigate(['/login']);
   }
 }
