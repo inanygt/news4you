c3d57d696e53e8b8da89bf071ac475af54d2cb43
JORIAN KASPRZAK <joriankasprzak@hotmail.com>
2023-02-02 12:46:17 +0100

    profile page update

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,5 +1,38 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-profile',
+//   templateUrl: './profile.component.html',
+//   styleUrls: ['./profile.component.css']
+// })
+// export class ProfileComponent {
+
+//   users: any;
+
+//   constructor() { }
+
+//   ngOnInit() {
+//     fetch('http://localhost:8000/api/users')
+//       .then(response => response.json())
+//       .then(data => {
+//         this.users = data;
+//         console.log(this.users)
+
+//         let user = JSON.parse(localStorage.getItem('user')!);
+//         console.log(user)
+//         console.log(user[0].userName);
+//         let username = user[0].userName
+//         console.log(username)
+
+//       });
+
+//   }
+
+// }
+
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +40,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
+  selectedTopics = {
+    sports: false,
+    politics: false,
+    war: false,
+    financial: false,
+    crypto: false,
+    tech: false,
+    science: false,
+    health: false,
+  };
+
+  saveChecked() {
+    localStorage.setItem('selectedTopics', JSON.stringify(this.selectedTopics));
+    // TODO: Send the data to the database
+  }
+
   user: any;
 
   users: any;
+
+
+
 
   constructor(private router: Router) {}
 
@@ -34,10 +86,15 @@ export class ProfileComponent implements OnInit {
         console.log(user[0].createdAt);
         this.user = user[0];
       });
+
+
   }
 
   logout() {
     localStorage.clear();
     this.router.navigate(['/login']);
-  }
+    }
+
+
+
 }
